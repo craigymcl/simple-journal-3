f663de4cb4ce4a6733090177771fa053e9febb74
Craig McLachlan <craigymcl@hotmail.com>
2019-05-07 09:54:49 +0100

    Added post featured image and added logo option.

--- src/_data/site.js
+++ src/_data/site.js
@@ -1,22 +1,23 @@
 /**
-  Your global data folder is controlled by the dir.data configuration option. 
-  All *.json and module.exports values from *.js files in this directory will 
+  Your global data folder is controlled by the dir.data configuration option.
+  All *.json and module.exports values from *.js files in this directory will
   be added into a global data object available to all templates.
 
   This file can be accessed using: {{ site.title }}
 */
 
 module.exports = {
-  title: "Hiking & Camping Log",
-  url: "https://log.chriscollins.me", // Don't end with a slash /
-  description: "Personal Hiking Log",
+  title: "Bokeh Boyzies | Photoblog",
+  logo: "bokehboyzies-logo.jpg",
+  url: "https://bokehboyzi.es", // Don't end with a slash /
+  description: "Just a couple of pals with a couple of cameras",
   copyright: {
-    from: "2003",
-    name: "Chris Collins"
+    from: "2019",
+    name: "Craig McLachlan and Stewart McKay"
   },
   social_meta: {
-    twitter: "@chrisssycollins",
-    instagram: "scottishstoater",
+    // twitter: "@chrisssycollins",
+    instagram: "bokehboyzies",
     featured_image: "/assets/images/featured_image.jpg"
   },
   ENV: process.env.ELEVENTY_ENV
